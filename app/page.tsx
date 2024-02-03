d8570dad34285b467d9038ab5aba6eaeeabf0032
virvaldev <valtteri.virtanen.dev@gmail.com>
2024-02-03 17:02:21 +0200

    CSS

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,21 @@ import React from 'react';
 
 const App: React.FC = () => {
   return (
-    <div className={styles.page}>
+    <main className={styles.main}>
       <h1 className={styles.title}>Hello World</h1>
-    </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input 
+          className={styles.input}
+            type='text' 
+            placeholder='What to do?' 
+            name='content'
+            required
+          />
+          <button className={styles.button}>Add</button>
+        </form>
+      </div>
+    </main>
   );
 }
 
